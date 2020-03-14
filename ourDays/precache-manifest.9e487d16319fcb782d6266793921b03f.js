d1f547c09b52dab45c76434fc59a993af3b66a7e
ianhsu831 <ian.xu831@gmail.com>
2020-03-14 12:11:05 +0800

    add 'our days' application

---
+++ ourDays/precache-manifest.9e487d16319fcb782d6266793921b03f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a329a5130c2e0a342a9081cbeedf358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2fd8f9537b438c6e5271fb443c549d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bad3c7cf992c94d7bb4a",
+    "url": "/src/css/app.11b993f8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/src/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4751a657a19edbcd9f67",
+    "url": "/src/js/about.f2ad3230.js"
+  },
+  {
+    "revision": "bad3c7cf992c94d7bb4a",
+    "url": "/src/js/app.afb426fe.js"
+  },
+  {
+    "revision": "d3e50ff17827d8548359",
+    "url": "/src/js/chunk-vendors.a89ae051.js"
+  }
+]);
