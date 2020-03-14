a229ab91479038a817795e314f84d9132449bb80
ianhsu831 <ian.xu831@gmail.com>
2020-03-14 18:32:40 +0800

    add main content

---
+++ ourDays/precache-manifest.496446dd8956b28ef8e443ca118d5ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eed89ab97cacf9cedd79e8268799251",
+    "url": "/ourDays/index.html"
+  },
+  {
+    "revision": "1ad0d487750e35aeefa1ca1704dafa67",
+    "url": "/ourDays/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ourDays/robots.txt"
+  },
+  {
+    "revision": "fbcd8457d0cf34eb0c87",
+    "url": "/ourDays/src/css/app.775bc4cb.css"
+  },
+  {
+    "revision": "fbcd8457d0cf34eb0c87",
+    "url": "/ourDays/src/js/app.7d397a72.js"
+  },
+  {
+    "revision": "64548c1c95d4989ae3ec",
+    "url": "/ourDays/src/js/chunk-vendors.0a456ad8.js"
+  }
+]);
